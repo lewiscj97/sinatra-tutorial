15d460f02e0744074938c1b7bc2545225dbe3fc7
Stella Martin <stellamartin222@gmail.com>
2021-10-04 15:11:54 +0100

    cat css complete

--- app.rb
+++ app.rb
@@ -1,2 +1,22 @@
-require 'sinatra'
+require "sinatra"
+require "sinatra/reloader" if development?
 
+get '/' do
+    "Hello World"
+end
+
+get '/secret' do
+    "this is the secret"
+end
+
+get '/stella' do
+    "I am really hungry"
+end
+
+get '/lewis' do
+    "I am really poorly"
+end
+
+get '/cat' do
+    "<img src = https://i.imgur.com/jFaSxym.png style='border : dashed red'></img>"
+end
